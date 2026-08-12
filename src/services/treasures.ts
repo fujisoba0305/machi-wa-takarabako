46ac810b1810e783850296305a1dede8b823c1f8
fujisoba <higepiyo0305@gmail.com>
2026-08-12 10:23:11 +0900

    宝物保存の診断ログを追加

--- src/services/treasures.ts
+++ src/services/treasures.ts
@@ -9,11 +9,25 @@ longitude: number;
 };
 
 export async function createTreasure(treasure: TreasureInsert) {
+console.info('[treasure-save] INSERT start', {
+namePresent: treasure.name.length > 0,
+categoryPresent: treasure.category.length > 0,
+latitudeType: typeof treasure.latitude,
+longitudeType: typeof treasure.longitude,
+});
+
 const { error } = await getSupabaseClient()
 .from('treasures')
 .insert(treasure);
 
 if (error) {
+console.error('[treasure-save] Supabase INSERT failed', {
+code: error.code,
+message: error.message,
+details: error.details,
+hint: error.hint,
+});
+
 throw new Error('Treasure insert failed');
 }
 }
